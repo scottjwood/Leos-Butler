70d3893e63dceca48ac78a755547a91751d9cd95
Scott Wood <scottjwood@users.noreply.github.com>
2024-05-19 17:14:38 -0600

    Simplify Nav

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -9,19 +9,6 @@ const NavBar = ({ user }) => {
         <li><Link to="/projects">Projects</Link></li>
         <li><Link to="/storage-locations">Storage Locations</Link></li>
         <li><Link to="/dashboard">Dashboard</Link></li>
-        {user ? (
-          <>
-            <li>Welcome, {user.username}</li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/settings">Settings</Link></li>
-            {/* Remove the Notifications link */}
-          </>
-        ) : (
-          <>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
-          </>
-        )}
       </ul>
     </nav>
   );
